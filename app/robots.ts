04201f092fbfabfc0428e7ae29db33585d723dec
r77tchan <r77tchan@gmail.com>
2026-04-14 09:53:33 +0900

    サイトマップインデックス手動追加

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,11 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/character-old/", "/scraping/", "/api/"],
     },
-    sitemap: "https://inazuma-eleven-db.vercel.app/sitemap.xml",
+    sitemap: [
+      "https://inazuma-eleven-db.vercel.app/sitemap.xml",
+      "https://inazuma-eleven-db.vercel.app/sitemap/0.xml",
+      "https://inazuma-eleven-db.vercel.app/sitemap/1.xml",
+      "https://inazuma-eleven-db.vercel.app/sitemap/2.xml",
+    ],
   };
 }
